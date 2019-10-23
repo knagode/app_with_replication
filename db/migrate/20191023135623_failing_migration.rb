5ad0a7b7edf04567287142476347b2e5fcfa977d
Klemen Nagode <klemen.nagode@gmail.com>
2019-10-23 16:26:29 +0200

    clarify problem in the code

--- db/migrate/20191023135623_failing_migration.rb
+++ db/migrate/20191023135623_failing_migration.rb
@@ -1,8 +1,7 @@
 class FailingMigration < ActiveRecord::Migration[6.0]
   def change
-
     add_column :my_models, :hidden, :boolean
 
-    MyModel.update_all(updated_at: Time.now)
+    MyModel.update_all(updated_at: Time.now) # MyModel uses: connects_to database: { writing: :primary, reading: :follower }
   end
 end
